3f03bb712f3483a2bcb8709cfa3bab71c82585fe
Simontagbor <simontagbor360@gmail.com>
2024-02-20 16:47:20 +0000

    Refactor Car class to use Serviceable, Engine, and Battery classes: Car class now inherits from Serviceable and uses Engine and Battery as components.

--- car.py
+++ car.py
@@ -1,10 +1,25 @@
-from abc import ABC, abstractmethod
+"""Defines the Car class."""
 
+from serviceable import Serviceable
+from engine.base_engine import Engine
+from battery.base_battery import Battery
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Car(Serviceable):
+    """Creates a Car object.
+    Returns: a car object with components like engine and battery
 
-    @abstractmethod
-    def needs_service(self):
-        pass
+    Usage:
+        >>> engine = Engine_type(......)
+        >>> battery = Battery_type(......)
+        >>> car_instance = Car(engine, battery)
+    """
+    def __init__(self, engine: Engine, battery: Battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self) -> bool:
+        """Check if Car needs to be serviced
+        The fucntion checks the service status of each of 
+        the components to determine if the car needs service
+        """
+        return self.engine.needs_service() or self.battery.needs_service()
