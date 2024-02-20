5dc9886a69d4d8915ae8e5013c127066ddbcf463
Simontagbor <simontagbor360@gmail.com>
2024-02-20 16:54:58 +0000

    Define Engine and Battery Base classes.*** both classes provide an abstract method for child Engine and Battery classes to implement. This approach is in line with the strategy design pattern where  a common method is provided by the parent class that can be modified depending on the strategy for determining whether a component should be serviced.

---
+++ battery/base_battery.py
@@ -0,0 +1,16 @@
+"""This module implements the Battery class,
+which is responsible for creating Battery objects."""
+
+
+from abc import ABC, abstractmethod
+
+
+class Battery(ABC):
+    """This class defines basic attributes of Batteries objects.
+
+    """
+    @abstractmethod
+    def needs_service(self):
+        """Determines if the batery should be serviced. 
+        This method Needs to be implemented by the subclass.
+        """
---
+++ engine/base_engine.py
@@ -0,0 +1,13 @@
+"""This module implements the Engine class, which is responsible for creating engine objects."""
+	
+from abc import ABC, abstractmethod
+
+class Engine(ABC):
+    """This class is responsible for creating different types of engine objects.
+
+    """
+    @abstractmethod
+    def needs_service(self):
+        """Determines if the engine should be serviced. 
+        This method Needs to be implemented by the subclass.
+        """
