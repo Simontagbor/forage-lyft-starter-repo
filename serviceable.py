918ca5075cdb36580ebbd5a601ff1013b9ddf6c9
Simontagbor <simontagbor360@gmail.com>
2024-02-20 16:43:44 +0000

    add  `serviceable.py` module to define the base class for the different types of vehicles.

---
+++ serviceable.py
@@ -0,0 +1,14 @@
+"""An interface for serviceable objects."""
+
+from abc import ABC, abstractmethod
+
+class Serviceable(ABC):
+    """An interface for serviceable objects.
+    """
+    @abstractmethod
+    def needs_service(self):
+        """Determines if the object needs to be serviced.
+        This method needs to be implemented by the subclass.
+        """
+        pass	
+
