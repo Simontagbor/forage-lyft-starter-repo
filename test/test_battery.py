9e348d8cafdfa9220815bab567176bc93e309bd7
Simontagbor <simontagbor360@gmail.com>
2024-02-26 20:43:24 +0000

    Add unit tests for Battery, and Engine Classes.
    - Added unittests for  NubbinBattery, and SpindlerBattery classes
    - Added Unittests for the willougby, capulet,  and sternman engines

---
+++ test/test_battery.py
@@ -0,0 +1,99 @@
+"""Unit Tests for the Base Battery and Derived Nubbin and Spindler Batteries
+"""
+
+import unittest
+from abc import ABC
+from datetime import datetime, timedelta
+from battery.base_battery import Battery
+from battery.nubbin import NubbinBattery
+from battery.spindler import SpindlerBattery
+
+from utils.input_validator import validate_date
+
+
+class TestBattery(unittest.TestCase):
+    """Tests for the Battery Class
+    This tests that the Battery base class is correctly implemented according to specification
+    """
+    def test_battery_is_subclass_of_abstract_base_class(self):
+        """Test that the Battery class is an instance of the ABC class"""
+        self.assertTrue(issubclass(Battery, ABC))
+
+    def test_battery_needs_service(self):
+        """Test that the Battery class has a needs_service method"""
+        self.assertTrue(hasattr(Battery, 'needs_service'))
+
+
+class TestNubbinBattery(unittest.TestCase):
+    """Tests for the NubbinBattery Class
+    This tests that the NubbinBattery class is correctly implemented
+    """
+    def test_nubbin_battery_is_subclass_of_battery(self):
+        """Test that the NubbinBattery class is an instance of the Battery class"""
+        self.assertTrue(issubclass(NubbinBattery, Battery))
+
+    def test_nubbin_battery_needs_service(self):
+        """Test that the NubbinBattery class has a needs_service method"""
+        self.assertTrue(hasattr(NubbinBattery, 'needs_service'))
+
+    def test_nubbin_battery_needs_service_returns_bool(self):
+        """Test that the needs_service method returns a boolean"""
+        current_date = validate_date(datetime.now())
+        last_service_date = validate_date(datetime.now())
+        self.assertIsInstance(NubbinBattery(last_service_date, current_date).needs_service(), bool)
+    
+    def test_nubbin_battery_needs_service_returns_true(self):
+        """Test that the needs_service method returns True when the battery needs service"""
+        current_date = validate_date(datetime.now())
+        base_year = 2024
+        current_year = datetime.now().year
+        year_difference = 6 + ((current_year - base_year) // 3)
+        last_service_date = current_date.replace(year=current_date.year - year_difference)
+        self.assertTrue(NubbinBattery(last_service_date, current_date).needs_service())
+
+    def test_nubbin_battery_needs_service_returns_false(self):
+        """Test that the needs_service method returns 
+        False when the battery does not need service"""
+        current_date = validate_date(datetime.now())
+        last_service_date = validate_date(datetime.now())
+        self.assertFalse(NubbinBattery(last_service_date, current_date).needs_service())
+
+
+class TestSpindlerBattery(unittest.TestCase):
+    """Tests for the SpindlerBattery Class
+    This tests that the SpindlerBattery class is correctly implemented
+    """
+    def test_spindler_battery_is_subclass_of_battery(self):
+        """Test that the SpindlerBattery class is an
+        instance of the Battery class"""
+        self.assertTrue(issubclass(SpindlerBattery, Battery))
+
+    def test_spindler_battery_needs_service(self):
+        """Test that the SpindlerBattery class has a needs_service method"""
+        self.assertTrue(hasattr(SpindlerBattery, 'needs_service'))
+
+    def test_spindler_battery_needs_service_returns_bool(self):
+        """Test that the needs_service method returns a boolean"""
+        current_date = validate_date(datetime.now())
+        last_service_date = validate_date(datetime.now())
+        self.assertIsInstance(SpindlerBattery(last_service_date, current_date).needs_service(), bool)
+
+    def test_spindler_battery_needs_service_returns_true(self):
+        """Test that the needs_service method returns True when the battery needs service"""
+        current_date = validate_date(datetime.now())
+        base_year = 2024
+        current_year = datetime.now().year
+        year_difference = 3 + ((current_year - base_year) // 3)
+        last_service_date = current_date.replace(year=current_date.year - year_difference)
+        self.assertTrue(SpindlerBattery(last_service_date, current_date).needs_service())
+
+    def test_spindler_battery_needs_service_returns_false(self):
+        """Test that the needs_service method
+        returns False when the battery does not need service"""
+        current_date = validate_date(datetime.now())
+        last_service_date = validate_date(datetime.now())
+        self.assertFalse(SpindlerBattery(last_service_date, current_date).needs_service())
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_engine.py
@@ -0,0 +1,102 @@
+"""Unit Tests for the Base Engine and specialised Engines classes
+This module contains unit tests for the Base Engine and specialised Engines"""
+
+import unittest
+from abc import ABC
+from engine.base_engine import Engine
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+
+class TestEngine(unittest.TestCase):
+    """Tests for the Engine Class
+    This tests that the Engine base class is correctly implemented according to specification
+    """
+    def test_engine_is_subclass_of_abstract_base_class(self):
+        """Test that the Engine class is an instance of the ABC class"""
+        self.assertTrue(issubclass(Engine, ABC))
+
+    def test_engine_needs_service(self):
+        """Test that the Engine class has a needs_service method"""
+        self.assertTrue(hasattr(Engine, 'needs_service'))
+
+
+class TestCapuletEngine(unittest.TestCase):
+    """Tests for the CapuletEngine Class
+    This tests that the CapuletEngine class is correctly implemented
+    """
+    def test_capulet_engine_is_subclass_of_engine(self):
+        """Test that the CapuletEngine class is an instance of the Engine class"""
+        self.assertTrue(issubclass(CapuletEngine, Engine))
+
+    def test_capulet_engine_needs_service(self):
+        """Test that the CapuletEngine class has a needs_service method"""
+        self.assertTrue(hasattr(CapuletEngine, 'needs_service'))
+
+    def test_capulet_engine_needs_service_returns_bool(self):
+        """Test that the needs_service method returns a boolean"""
+        self.assertIsInstance(CapuletEngine(0, 0).needs_service(), bool)
+
+    def test_capulet_engine_needs_service_returns_true(self):
+        """Test that the needs_service method returns True when the engine needs service"""
+        self.assertTrue(CapuletEngine(30001, 0).needs_service())
+
+    def test_capulet_engine_needs_service_returns_false(self):
+        """Test that the needs_service method returns False when the engine does not need service"""
+        self.assertFalse(CapuletEngine(30000, 0).needs_service())
+
+
+class TestSternmanEngine(unittest.TestCase):
+    """Tests for the SternmanEngine Class
+    This tests that the SternmanEngine class is correctly implemented
+    """
+    def test_sternman_engine_is_subclass_of_engine(self):
+        """Test that the SternmanEngine class is an instance of the Engine class"""
+        self.assertTrue(issubclass(SternmanEngine, Engine))
+
+    def test_sternman_engine_needs_service(self):
+        """Test that the SternmanEngine class has a needs_service method"""
+        self.assertTrue(hasattr(SternmanEngine, 'needs_service'))
+
+    def test_sternman_engine_needs_service_returns_bool(self):
+        """Test that the needs_service method returns a boolean"""
+        self.assertIsInstance(SternmanEngine(True).needs_service(), bool)
+
+    def test_sternman_engine_needs_service_returns_true(self):
+        """Test that the needs_service method returns True when the engine needs service"""
+        self.assertTrue(SternmanEngine(True).needs_service())
+
+    def test_sternman_engine_needs_service_returns_false(self):
+        """Test that the needs_service method returns False when the engine does not need service"""
+        self.assertFalse(SternmanEngine(False).needs_service())
+
+
+class TestWilloughbyEngine(unittest.TestCase):
+    """Tests for the WilloughbyEngine Class
+    This tests that the WilloughbyEngine class is correctly implemented
+    """
+    def test_willoughby_engine_is_subclass_of_engine(self):
+        """Test that the WilloughbyEngine class is an instance of the Engine class"""
+        self.assertTrue(issubclass(WilloughbyEngine, Engine))
+
+    def test_willoughby_engine_needs_service(self):
+        """Test that the WilloughbyEngine class has a needs_service method"""
+        self.assertTrue(hasattr(WilloughbyEngine, 'needs_service'))
+
+    def test_willoughby_engine_needs_service_returns_bool(self):
+        """Test that the needs_service method returns a boolean"""
+        self.assertIsInstance(WilloughbyEngine(0, 0).needs_service(), bool)
+
+    def test_willoughby_engine_needs_service_returns_true(self):
+        """Test that the needs_service method returns True when the engine needs service"""
+        self.assertTrue(WilloughbyEngine(60001, 0).needs_service())
+
+    def test_willoughby_engine_needs_service_returns_false(self):
+        """Test that the needs_service method returns False when the engine does not need service"""
+        self.assertFalse(WilloughbyEngine(60000, 0).needs_service())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
